02cdfcec02540ad32ff79bf9d7402da117d75a73
Liza <elizavetastudenikina@gmail.com>
2016-03-15 13:05:05 +0300

    Added files via upload

--- cat/cat.c
+++ cat/cat.c
@@ -1,36 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <errno.h>
 #include <fcntl.h>
+#include <errno.h>
 
 #define BUFFER_SIZE 256
 
-int m_write(int fd) 
+void m_write(char* buffer, int fd) 
 {
-	char buffer[BUFFER_SIZE];
-	ssize_t res;
-	while ((res = read(fd, buffer, BUFFER_SIZE)))
-	{
-		ssize_t res_w = write(1, buffer, res);
-		if (res_w < res) 
-		    return -1;
+	ssize_t rres;
+	while ((rres = read(fd, buffer, BUFFER_SIZE)) > 0)
+	{ 
+		ssize_t wres = rres, res;
+		while (wres > 0 && (res = write(1, buffer + (rres - wres), wres))) 			{
+			if (res < 0) 
+			{
+				perror("Error during writing");
+				continue;
+				//if (errno == EINTER) continue;
+				//return;
+			}
+			wres = rres -res;
+		}
 		
 	}
-	return 0;
 }
 
 int main (int argc, char *argv[])
 {
+	char buffer[BUFFER_SIZE];
 	if (argc > 1) 
     	{
 		size_t i;
 		for (i = 1; i < argc; ++i) 
 		{
 			int fd = open(argv[i], O_RDONLY);
-			if (m_write(fd) == -1) 
-				perror("Err");
+			if (fd == -1) 
+			{
+				continue;
+			}			
+			m_write(buffer, fd);
 	   	 	close(fd);
       		 }
-    	}
+    	} else if (argc == 1) m_write(buffer, 0);
+	return 0;
 }
+
+
